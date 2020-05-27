752229a0056d61c4af05d9c4e1089fc7142bfdeb
panzerstadt <tliqun@gmail.com>
2020-05-27 13:32:34 +0900

    add jp font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      sans: ["Arial", "Helvetica", "sans-serif"],
+      serif: ["Noto Serif JP", "monospace"], // FIXME: replace monosapce with "serif" when prod
+    },
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
