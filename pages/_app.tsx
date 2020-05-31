eac6c95d614c87b6bc9b995fc9013e83d96d3541
panzerstadt <tliqun@gmail.com>
2020-06-01 00:32:59 +0900

    move shopify client init to parent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,17 @@ import "../styles/index.css";
 import { AppProvider } from "@shopify/polaris";
 import enTranslations from "@shopify/polaris/locales/en.json";
 
+import Client from "shopify-buy";
+
+const client = Client.buildClient({
+  domain: "takumai.myshopify.com/",
+  storefrontAccessToken: "4800632e5ebc937a33a9ea3d6bc158a6",
+});
+
 const AppRoot = ({ Component, pageProps }) => {
   return (
     <AppProvider i18n={enTranslations}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} client={client} />;
     </AppProvider>
   );
 };
