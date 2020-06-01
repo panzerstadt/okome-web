c6b675dfd826ee7d9ce55fed821aea179e18d63e
matt-writes-code <37174946+mayojich@users.noreply.github.com>
2020-06-01 11:35:08 +0800

    'refactored-formspree-to-hooks'

--- pages/formspree-test.tsx
+++ pages/formspree-test.tsx
@@ -1,33 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default class MyForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.submitForm = this.submitForm.bind(this);
-    this.state = {
-      status: "",
-    };
-  }
+const Inquiries = () => {
+  const [inquiries, setInquiries] = useState({});
 
-  render() {
-    const { status } = this.state;
-    return (
-      <form
-        onSubmit={this.submitForm}
-        action="https://formspree.io/xrgypnwo"
-        method="POST"
-      >
-        <label>Email:</label>
-        <input type="email" name="email" />
-        <label>Message:</label>
-        <input type="text" name="message" />
-        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
-        {status === "ERROR" && <p>Ooops! There was an error.</p>}
-      </form>
-    );
-  }
-
-  submitForm(ev) {
+  function submitForm(ev) {
     ev.preventDefault();
     const form = ev.target;
     const data = new FormData(form);
@@ -38,11 +14,28 @@ export default class MyForm extends React.Component {
       if (xhr.readyState !== XMLHttpRequest.DONE) return;
       if (xhr.status === 200) {
         form.reset();
-        this.setState({ status: "SUCCESS" });
+        setInquiries({ inquiries: "SUCCESS" });
       } else {
-        this.setState({ status: "ERROR" });
+        setInquiries({ inquiries: "ERROR" });
       }
     };
     xhr.send(data);
   }
-}
+
+  return (
+    <form
+      onSubmit={submitForm}
+      action="https://formspree.io/xrgypnwo"
+      method="POST"
+    >
+      <label>Email:</label>
+      <input type="email" name="email" />
+      <label>Message:</label>
+      <input type="text" name="message" />
+      {inquiries === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+      {inquiries === "ERROR" && <p>Ooops! There was an error.</p>}
+    </form>
+  );
+};
+
+export default Inquiries;
