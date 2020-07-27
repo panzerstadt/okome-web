dc53c2d057e9ecf5303be2a14906ce44d233366f
matt-writes-code <37174946+mayojich@users.noreply.github.com>
2020-07-27 18:17:10 +0800

    'additional-SectionHeader'

--- pageComponents/Page2.tsx
+++ pageComponents/Page2.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { useIntersectionObserver } from "../components/hooks/useIntersectionObserver";
-
+import SectionHeader from "../components/SectionHeader";
 export const Page2 = () => {
   const [ref, entry] = useIntersectionObserver({});
 
@@ -12,21 +12,22 @@ export const Page2 = () => {
   return (
     <div className="h-screen bg-cyan">
       <div className="flex flex-col items-center justify-center h-full">
-        <motion.h1
-          ref={ref}
-          className="p-12 leading-normal text-center sm:p-16"
-          variants={variants}
-          initial={"hidden"}
-          animate={entry.intersectionRatio > 0 ? "visible" : "hidden"}
-          transition={{
-            duration: 0.6,
-            ease: [0.175, 0.885, 0.32, 1.275],
-          }}
-        >
-          宅米便とは…
-        </motion.h1>
+        <SectionHeader title="宅米便とは…" />
         <div className="flex flex-row items-center">
           <img src="/images/home/okomekun.png" className="h-128" />
+          <motion.h3
+            ref={ref}
+            className="p-12 leading-normal text-center sm:p-16"
+            variants={variants}
+            initial={"hidden"}
+            animate={entry.intersectionRatio > 0 ? "visible" : "hidden"}
+            transition={{
+              duration: 0.6,
+              ease: [0.175, 0.885, 0.32, 1.275],
+            }}
+          >
+            ごはんどーぞ♪
+          </motion.h3>
         </div>
         <h3 className="px-10 py-10">
           宅米便は 「お米くん」 が営むお米の宅配サービスです。
